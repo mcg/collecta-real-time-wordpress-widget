444d88e339e7d17f759c1aeadcf796161d92c44d
Matthew Gregg <mcg@braintube.com>
2010-02-03 20:20:27 -0500

    bump version

--- collecta_widget.php
+++ collecta_widget.php
@@ -1,7 +1,7 @@
 <?php
   /*
    * Plugin Name: Collecta Search Widget
-   * Version: 0.5
+   * Version: 0.6
    * Plugin URI: http://widget.collecta.com/
    * Description: Collecta.com Real-time widget.
    * Author: Matthew Gregg, Mick Thompson
